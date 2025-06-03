59a9014039e5497fd9462d890d0612883f7bb52c
Allen Mwandunga <allenmcndounger@gmail.com>
2025-06-03 13:40:43 +0200

    revamp endpoint uri since api-gateway will append the api/ automatically

--- drop-points-service/dropPointsSys/src/main/java/de/fhdo/dropPointsSys/controllers/droppoint/DropPointRestController.java
+++ drop-points-service/dropPointsSys/src/main/java/de/fhdo/dropPointsSys/controllers/droppoint/DropPointRestController.java
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/api/droppoints")
+@RequestMapping("/droppoints")
 public class DropPointRestController {
 
     private final DropPointService dropPointService;
