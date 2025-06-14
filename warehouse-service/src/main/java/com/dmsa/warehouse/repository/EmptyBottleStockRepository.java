e8106f4944e863fe45e8a9eb4ffdd32d43d54e07
Bhavana Shivaraju <bhavana28sp@gmail.com>
2025-06-15 00:09:54 +0200

    Frontend related changes - 1

--- warehouse-service/src/main/java/com/dmsa/warehouse/repository/EmptyBottleStockRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dmsa.warehouse.repository;
-
-import com.dmsa.warehouse.model.EmptyBottleStock;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmptyBottleStockRepository extends JpaRepository<EmptyBottleStock, Long> { }
