4d74511c85215bfcdba0bb910ea54655b8babcc0
Daranya Maniseharan <65844372+Daranya@users.noreply.github.com>
2025-06-24 15:09:25 +0200

    server changes for event planner

--- event-planner-service/frontend/vue.config.js
+++ event-planner-service/frontend/vue.config.js
@@ -1,7 +1,7 @@
 // vue.config.js
 module.exports = {
   devServer: {
-    port: 3000,             // ← serve your Vue app on 3000
+    port: 5173,             // ← serve your Vue app on 3000
     proxy: {
       '/api': {
         target: 'http://localhost:8080',  // your Spring Boot
